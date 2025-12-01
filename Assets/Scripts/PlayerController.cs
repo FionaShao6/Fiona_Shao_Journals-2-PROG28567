811575a14682242a5e00f716e5a2ea723000d7c2
FionaShao6 <fionashao6@gmail.com>
2025-12-01 17:04:33 -0500

    Final Deliverable-Dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,15 @@ public class PlayerController : MonoBehaviour
     public float coyoteTime = 0.1f;
     public float coyoteTimeCounter;
 
+    public float dashMultiplier = 2f; // dash speed multiplier
+    public float dashDuration = 0.3f; 
+    public float dashCooldown = 1f; // dash cooldown time£¬ 1 second, to prevent infinite dashing
+    public float dashCooldownTimer = 0f;//Current remaining cooldown time
+    private bool isDashing = false; // Is the player dashing?
+    public LayerMask enemyLayer; // used to set penetration
+    public float targetSpeed;
+
+   
 
     public enum FacingDirection
     {
@@ -36,10 +45,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //rb.gravityScale = 0;
+        controllerRB =GetComponent<Rigidbody2D>();
         gravity = -2 * apexHeight / (Mathf.Pow(apexTime,2));
-        initialJumpVelocity = 2 * apexHeight / apexTime; 
-
+        initialJumpVelocity = 2 * apexHeight / apexTime;
+        
     }
 
     // Update is called once per frame
@@ -70,13 +79,40 @@ public class PlayerController : MonoBehaviour
             }
 
         }
+
+        bool isHoldingDirection = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f;
+        //Whether it's to the left or right, it can be detected using absolute values.
+
+        bool isPressingShift = Input.GetKeyDown(KeyCode.LeftShift);
+
+        bool canDash = !isDashing && isHoldingDirection&& dashCooldownTimer <= 0f;
+        //Check if you can dash: Not currently dashting + Hold down the direction key
+      
+       
+        if (isPressingShift && canDash)
+        {
+            StartDash();
+        }
+
+        if (dashCooldownTimer > 0f)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
         if (rb == null) {return;}
 
-        Vector2 targetVelocity = new Vector2(playerInput.x * maxSpeed, rb.linearVelocity.y);
+        if (isDashing == true)
+        {
+            targetSpeed = maxSpeed * dashMultiplier;
+        }
+        else
+        {
+            targetSpeed = maxSpeed ;
+        }
+        Vector2 targetVelocity = new Vector2(playerInput.x * targetSpeed, rb.linearVelocity.y);
 
         rb.linearVelocity = Vector2.MoveTowards(rb.linearVelocity,targetVelocity,acceleration * Time.fixedDeltaTime);
         if (playerInput.x > 0.1f)
@@ -87,7 +123,7 @@ public class PlayerController : MonoBehaviour
         {
             currentFacing = FacingDirection.left;
         }
-
+         
 
 
     }
@@ -131,10 +167,27 @@ public class PlayerController : MonoBehaviour
         return currentFacing;
     }
 
-   /*public void Jump()
+
+    public void StartDash()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, initialJumpVelocity);
+        isDashing = true;
+        dashCooldownTimer = dashCooldown;
+        //Activating the cooldown is equivalent to dashing,
+        //the cooldown immediately becomes 1 second and the countdown begins.
+        
+        int enemyLayerIndex = LayerMask.NameToLayer("Enemy");
+        Physics2D.IgnoreLayerCollision(gameObject.layer, enemyLayerIndex, true);
+        Invoke("EndDash", dashDuration);
+        //The EndDash() method will be executed automatically after dashDuration seconds.
+
     }
-  
-    */
+    public void EndDash()
+    {
+        isDashing = false;
+        int enemyLayerIndex = LayerMask.NameToLayer("Enemy");
+        Physics2D.IgnoreLayerCollision(gameObject.layer, enemyLayerIndex, false);
+        Debug.Log("finish dash");
+       
+    }
+
 }
